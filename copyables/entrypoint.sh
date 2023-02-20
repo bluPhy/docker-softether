7f7f354317fb6a2ad34e3fa73be981b0f41d5362
Alejandro Leal <ajleal@msn.com>
2023-02-20 00:36:38 -0500

    v4.41-9782-beta-2022.11.17
    v4.41-9782-beta-2022.11.17

--- copyables/entrypoint.sh
+++ copyables/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Disable THP Support in kernel
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never >/sys/kernel/mm/transparent_hugepage/enabled
 # TCP backlog setting (defaults to 128)
 sysctl -w net.core.somaxconn=16384
 #---------------------------------------------------------------
